1a1875979c5071938927f91a2607dc7278e0eddc
Zachary Cotton <mr.zacharycotton@gmail.com>
2020-07-11 00:48:10 -0700

    Code Synced between different tabs

--- srv.js
+++ srv.js
@@ -1,9 +1,30 @@
 express = require("express")
+http = require("http")
+socketio = require("socket.io")
 
 app = express()
+srv = http.createServer(app)
+io = socketio(srv)
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname+"/index.html")
 })
 
-app.listen(8000, () => "Started")
+file = "A dog has floopyy ears"
+
+io.on("connection", function(client) {
+  client.on("get", function(data) {
+    client.emit("file", file)
+  })
+  client.on("edit", function(data) {
+    console.log(data)
+    if (!data.backspace) {
+        file += data.value;
+    } else {
+      file = file.slice(0,-1)
+    }
+    client.broadcast.emit("update", data)
+  })
+})
+
+srv.listen(8000)
