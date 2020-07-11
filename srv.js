d31daa309d1d1147bc06b8d5feb446ce3ffe1161
Zachary Cotton <mr.zacharycotton@gmail.com>
2020-07-11 01:43:45 -0700

    Running Code

--- srv.js
+++ srv.js
@@ -1,30 +1,44 @@
 express = require("express")
 http = require("http")
 socketio = require("socket.io")
+exec = require('child_process').exec
+fs = require('fs')
 
 app = express()
 srv = http.createServer(app)
 io = socketio(srv)
 
+file = "<insert code here>"
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname+"/index.html")
 })
-
-file = "<insert code here>"
 
 io.on("connection", function(client) {
   client.on("get", function(data) {
     client.emit("file", file)
   })
+
   client.on("edit", function(data) {
-    console.log(data)
-    if (!data.backspace) {
-        file += data.value;
-    } else {
-      file = file.slice(0,-1)
-    }
-    client.broadcast.emit("update", data)
+    file = data
+    client.broadcast.emit("file", data)
   })
+
+  client.on("run", function(data) {
+    fs.writeFile("prog.py", file, function(err) {
+      exec("python prog.py", function(error, stdout, stderr){
+        client.emit("results", {
+          output: stdout,
+          error: stderr
+        })
+        client.broadcast.emit("results", {
+          output: stdout,
+          error: stderr
+        })
+      })
+    })
+  })
+
 })
 
-srv.listen(8000)
+srv.listen(8001)
